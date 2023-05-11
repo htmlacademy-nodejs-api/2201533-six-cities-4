f4f18450820774076d2a1c4e24897a544f3a7dd4
OEM Configuration (temporary user) <89179699365@mail.ru>
2023-05-11 15:36:17 +0400

    progress

--- src/core/cli-command/progress.command.ts
+++ src/core/cli-command/progress.command.ts
@@ -1,20 +1,21 @@
 import {CliCommandInterface} from './cli-command.interface.js';
 import { stdout as output } from 'node:process';
-import {progressBar} from '../helpers/progress-bar.js';
+import {ProgressBar} from '../helpers/progress-bar.js';
+import {formatInt} from '../helpers/format-int.js';
 
 export default class ProgressCommand implements CliCommandInterface {
   public readonly name = '--progress';
   public async execute(): Promise<void> {
     let i = 0;
     const max = 128;
+    const progressBar = new ProgressBar(max);
+    console.log(progressBar.getProgress(0));
     const interval = setInterval(() => {
-      console.log(progressBar(++ i, max));
+      output.moveCursor(0, -1);
+      console.log(` ${progressBar.getProgress(++ i)} ${formatInt(i)} из ${formatInt(max)}`);
       if (i === 128) {
         clearInterval(interval);
       }
     }, 200);
-    console.log('Строка1');
-    output.moveCursor(0, -1);
-    console.log('Строка2');
   }
 }
--- src/core/helpers/progress-bar.ts
+++ src/core/helpers/progress-bar.ts
@@ -1,26 +1,43 @@
-export function progressBar(progress: number, max: number) {
-  const width = 10;
-  const step = Math.round(max / width);
-  console.log(`step: ${step}`);
-  const steps = Math.floor(progress / step);
-  console.log(`steps: ${steps}`);
-  const tail = progress % step;
-  console.log(`tail: ${tail}`);
-  return progress.toString();
-  // return `${[
-  //   String.fromCharCode(9608),
-  //   String.fromCharCode(9609),
-  //   String.fromCharCode(9610),
-  //   String.fromCharCode(9611),
-  //   String.fromCharCode(9612),
-  //   String.fromCharCode(9613),
-  //   String.fromCharCode(9614),
-  //   String.fromCharCode(9615),
-  //   String.fromCharCode(9621),
-  //   String.fromCharCode(9620),
-  //   String.fromCharCode(9615),
-  //   String.fromCharCode(9621),
-  //   String.fromCharCode(9601),
-  //   String.fromCharCode(9615),
-  // ].join('')}`;
+const FRACTIONS_COUNT = 8;
+const FULL_STEP = String.fromCharCode(9608);
+const FRACTIONS = [
+  ' ',
+  String.fromCharCode(9615),
+  String.fromCharCode(9614),
+  String.fromCharCode(9613),
+  String.fromCharCode(9612),
+  String.fromCharCode(9611),
+  String.fromCharCode(9610),
+  String.fromCharCode(9609),
+  String.fromCharCode(9608)
+];
+
+export class ProgressBar {
+  private readonly width: number = 10;
+  private readonly max: number;
+  private readonly step: number;
+  private readonly littleStep: number;
+  private emptyProgress: string[];
+  constructor (max: number, width?: number) {
+    this.max = max;
+    if (width) {
+      this.width = width;
+    }
+    this.step = this.max / this.width;
+    this.littleStep = this.step / FRACTIONS_COUNT;
+    this.emptyProgress = new Array(this.width).fill(' ');
+  }
+
+  public getProgress (progress: number) {
+    const stepsCount = Math.floor(progress / this.step);
+    return Array.from(this.emptyProgress, ((element, index) => {
+      if (index < stepsCount) {
+        return FULL_STEP;
+      }
+      if (index === stepsCount) {
+        return FRACTIONS[Math.round((progress - stepsCount * this.step) / this.littleStep)];
+      }
+      return element;
+    })).join('');
+  }
 }
