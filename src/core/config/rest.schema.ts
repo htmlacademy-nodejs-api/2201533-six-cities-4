8508e2d42e1690f55f5847b949777f9ce013f05a
Rimich666 <89179699365@mail.ru>
2023-05-17 18:08:50 +0400

    Архитектура приложения

---
+++ src/core/config/rest.schema.ts
@@ -0,0 +1,31 @@
+import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+export type RestSchema = {
+  PORT: number;
+  DB_HOST: string;
+  SALT: string;
+}
+
+convict.addFormats(validator);
+
+export const configRestSchema = convict<RestSchema>({
+  PORT: {
+    doc: 'Port for incoming connections',
+    format: 'port',
+    env: 'PORT',
+    default: 4000
+  },
+  DB_HOST: {
+    doc: 'IP address to connect to the database',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  },
+  SALT: {
+    doc: 'Cryptographic salt',
+    format: String,
+    env: 'SALT',
+    default: null
+  }
+});
