6ce229dd4a4b9cea2a7694d0a7d16872d7405755
OEM Configuration (temporary user) <89179699365@mail.ru>
2023-06-08 15:12:34 +0400

    multer 3

--- src/modules/middlewares/raw-request.middleware.ts
+++ src/modules/middlewares/raw-request.middleware.ts
@@ -4,7 +4,7 @@ import express, {NextFunction, Request, Response} from 'express';
 export class RawRequestMiddleware implements MiddlewareInterface {
 
   public async execute(req: Request, _res: Response, next: NextFunction): Promise<void> {
-    const raw = express.raw({limit: 100000000, type: 'multipart/form-data'});
+    const raw = express.text({limit: 100000000, type: 'multipart/form-data'});
     raw(req, _res, next);
   }
 }
